528e0d174999c73481acb2da61d223b0b876311c
Luke McConnell <lukermcconnell@gmail.com>
2022-08-17 13:15:10 +0900

    js refactor

--- particles.js
+++ particles.js
@@ -1,39 +1,154 @@
-let numParticles = 0;
-let positions = [0,25,50,75,100];
-let animationType = "";
+let animType;
+let numParticles;
 
-function launchAnimation(animType) {
-    animationType = animType;
-    console.log(animationType + " Animation Launched");
-    addParticle(100);
+let positions = [0, 25, 50, 75, 100];
+let postitionOpacity = [1, .8, .5, .3, 0];
+let postitionX = [], postitionY = [];
+let bckgrdColors = [], blurColors = [];
+let animTransX = [], animTransY = [];
+let animOpac = [], animColor = [], animBlur = [];
+
+function launchAnimation(animChoice, numPart) {
+
+    animType = animChoice;
+    numParticles = numPart;
+
+    initAnimFields()
+    setColors();
+    
+    console.log(animType + " Animation Launching");
+    addParticles(); 
     runAnimation();
 }
 
-function runAnimation(){
-    for(let i = 0; i < numParticles; i++){
-            updateParticles(i,animationType);
+function setColors() {
+    switch (animType) {
+        case "clockwise":
+            break;
+        case "up":
+            bckgrdColors = ["#e3dede", "#bab6b6", "#737070", "#383838", "#121212"];
+            blurColors = ["#e3dede", "#bab6b6", "#737070", "#383838", "#121212"];
+            break;
+        case "down":
+            bckgrdColors = ["#336bc4", "#2a58a1", "#20447d", "#0d3678", "#082554"];
+            blurColors = ["#2cb1bf", "#2797a3", "#1f7680", "#145961", "#09464d"];
+            break;
+        default:
+            break;
     }
 }
 
-function addParticle(numberParticlesCreate){
-    for(let i = 0; i < numberParticlesCreate; i++){
+function initAnimFields() {
+    for (let i = 0; i < positions.length; i++) {
+        animTransX.push('--animation-translateX-' + positions[i]);
+        animTransY.push('--animation-translateY-' + positions[i]);
+        animOpac.push('--animation-opacity-' + positions[i]);
+        animColor.push('--animation-color-' + positions[i]);
+        animBlur.push('--animation-blur-' + positions[i]);
+    }
+}
+
+function runAnimation() {
+    for(let i = 0; i < numParticles; i++) {
+            updateParticles(i, animType);
+    }
+}
+
+function addParticles() {
+    let particleContainer = document.querySelector('.particle-container');
+
+    for(let i = 0; i < numParticles; i++) {
         let newParticle = document.createElement("div");
         newParticle.className = "particle";
         newParticle.id = i;
-        let particleContainer = document.querySelector('.particle-container');
         particleContainer.appendChild(newParticle);
-        numParticles++;
     }
-    
 }
 
-function updateParticles(particleID, animationType){
+function updateParticles(particleID, animChoice) {
     let particleToUpdate = document.getElementById(particleID);
+
     updateAnimationTime(particleToUpdate);
-    if (animationType == "clockwise") updateClockwiseParticle(particleToUpdate);
-    if (animationType == "up") updateUpParticle(particleToUpdate);
-    if (animationType == "down") updateDownParticle(particleToUpdate);
+
+    switch (animChoice) {
+        case "clockwise":
+            updateClockwiseParticle(particleToUpdate);
+            break;
+        case "up":
+            updateUpParticle(particleToUpdate);
+            break;
+        case "down":
+            updateDownParticle(particleToUpdate);
+            break;
+        default:
+            break;
+    }
+}
+
+function updateClockwiseParticle(particleToUpdate) {
+    updatePositions();
+    updateOpacity();
+
+    for(let i = 0; i < positions.length; i++) {
+        particleToUpdate.style.setProperty(animTransX[i], postitionX[i] + "px");
+        particleToUpdate.style.setProperty(animTransY[i], postitionY[i] + "px");
+        particleToUpdate.style.setProperty(animOpac[i], postitionOpacity[i]);
+    }
+}
+
+function updateUpParticle(particleToUpdate) {
+    updatePositions();
+
+    for(let i = 0; i < positions.length; i++) {
+        particleToUpdate.style.setProperty(animTransX[i], postitionX[i] + "px");
+        particleToUpdate.style.setProperty(animTransY[i], postitionY[i] + "px");
+        particleToUpdate.style.setProperty(animOpac[i], postitionOpacity[i]);
+        particleToUpdate.style.setProperty(animColor[i], bckgrdColors[i]);
+        particleToUpdate.style.setProperty(animBlur[i], blurColors[i]);
+    }
+}
+
+function updateDownParticle(particleToUpdate) {
+    updatePositions();
     
+    particleToUpdate.style.setProperty("animation-timing-function", "linear");
+    for(let i = 0; i < positions.length; i++) {
+        particleToUpdate.style.setProperty(animTransX[i], postitionX[i] + "px");
+        particleToUpdate.style.setProperty(animTransY[i], postitionY[i] + "px");
+        particleToUpdate.style.setProperty(animOpac[i], postitionOpacity[i]);
+        particleToUpdate.style.setProperty(animColor[i], bckgrdColors[i]);
+        particleToUpdate.style.setProperty(animBlur[i], blurColors[i]);
+    }
+}
+
+function updatePositions() {
+    switch (animType) {
+        case "clockwise":
+            postitionX = [getRandom(0,700), getRandom(700,1400), getRandom(700,1400), getRandom(0,700), getRandom(0,700)];
+            postitionY = [getRandom(0,450), getRandom(0,450), getRandom(450,900), getRandom(450,900), getRandom(0,450)];
+            break;
+        case "up":
+            postitionX = [getRandom(0,1400), getRandom(0,1400), getRandom(0,1400), getRandom(0,1400), getRandom(0,1400)];
+            postitionY = [900, 700, 500, 300, 100];
+            break;
+        case "down":
+            let downX = getRandom(0,1400);
+            postitionX = [downX+50, downX+50, downX+50, downX+50, downX+50];
+            postitionY = [100, 300, 500, 700, 900];
+            break;
+        default:
+            break;
+    }
+}
+
+function updateOpacity() {
+    switch (animType) {
+        case "clockwise":
+            postitionOpacity = [Math.random(), Math.random(), Math.random(), Math.random()];
+            break;
+        default:
+            break;
+    }
 }
 
 function getRandom(min, max) {
@@ -42,68 +157,12 @@ function getRandom(min, max) {
 
 function updateAnimationTime(particleToUpdate) {
     let time = 10 * Math.random() + 2; 
-
     particleToUpdate.style.setProperty('--animation-time', time +'s');
 }
 
-function updateClockwiseParticle(particleToUpdate){
-    let postitionTranslateX = [getRandom(0,700),getRandom(700,1400),getRandom(700,1400),getRandom(0,700),getRandom(0,700)];
-    let postitionTranslateY = [getRandom(0,450),getRandom(0,450),getRandom(450,900),getRandom(450,900),getRandom(0,450)];
-    let postitionOpacity = [Math.random(),Math.random(),Math.random(),Math.random()];
 
-    for(let i = 0; i < positions.length; i++){
-        let animateTranslateXVar = '--animation-translateX-' + positions[i];
-        let animateTranslateYVar = '--animation-translateY-' + positions[i];
-        let animateOpacVar = '--animation-opacity-' + positions[i];
-        particleToUpdate.style.setProperty(animateTranslateXVar, postitionTranslateX[i] + "px");
-        particleToUpdate.style.setProperty(animateTranslateYVar, postitionTranslateY[i] + "px");
-        particleToUpdate.style.setProperty(animateOpacVar, postitionOpacity[i]);
-    }
-}
-function updateUpParticle(particleToUpdate){
-    let postitionTranslateX = [getRandom(0,1400),getRandom(0,1400),getRandom(0,1400),getRandom(0,1400),getRandom(0,1400)];
-    let postitionTranslateY = [900,700,500,300,100];
-    let postitionOpacity = [1,.8,.5,.3,0];
-    let bckgrdColors = ["#e3dede","#bab6b6","#737070","#383838","#121212"];
-    let blurColors = ["#e3dede","#bab6b6","#737070","#383838","#121212"];
-
-    for(let i = 0; i < positions.length; i++){
-        let animateTranslateXVar = '--animation-translateX-' + positions[i];
-        let animateTranslateYVar = '--animation-translateY-' + positions[i];
-        let animateOpacVar = '--animation-opacity-' + positions[i];
-        let animateColorVar = '--animation-color-' + positions[i];
-        let animateBlurVar = '--animation-blur-' + positions[i];
-        particleToUpdate.style.setProperty(animateTranslateXVar, postitionTranslateX[i] + "px");
-        particleToUpdate.style.setProperty(animateTranslateYVar, postitionTranslateY[i] + "px");
-        particleToUpdate.style.setProperty(animateOpacVar, postitionOpacity[i]);
-        particleToUpdate.style.setProperty(animateColorVar, bckgrdColors[i]);
-        particleToUpdate.style.setProperty(animateBlurVar, blurColors[i]);
-    }
-}
-
-function updateDownParticle(particleToUpdate){
-    let downX = getRandom(0,1400);
-    let postitionTranslateX = [downX+50,downX+50,downX+50,downX+50,downX+50];
-    let postitionTranslateY = [100,300,500,700,900];
-    let postitionOpacity = [1,.8,.5,.3,0];
-    let bckgrdColors = ["#336bc4","#2a58a1","#20447d","#0d3678","#082554"];
-    let blurColors = ["#2cb1bf","#2797a3","#1f7680","#145961","#09464d"];
-    particleToUpdate.style.setProperty("animation-timing-function", "linear");
-    for(let i = 0; i < positions.length; i++){
-        let animateTranslateXVar = '--animation-translateX-' + positions[i];
-        let animateTranslateYVar = '--animation-translateY-' + positions[i];
-        let animateOpacVar = '--animation-opacity-' + positions[i];
-        let animateColorVar = '--animation-color-' + positions[i];
-        let animateBlurVar = '--animation-blur-' + positions[i];
-        particleToUpdate.style.setProperty(animateTranslateXVar, postitionTranslateX[i] + "px");
-        particleToUpdate.style.setProperty(animateTranslateYVar, postitionTranslateY[i] + "px");
-        particleToUpdate.style.setProperty(animateOpacVar, postitionOpacity[i]);
-        particleToUpdate.style.setProperty(animateColorVar, bckgrdColors[i]);
-        particleToUpdate.style.setProperty(animateBlurVar, blurColors[i]);
-    }
-}
-
-function clearAnimation(){
+function clearAnimation() {
+    console.log("Clearing " + animType + " Animation");
     let particleContainer = document.querySelector('.particle-container');
     while (particleContainer.lastElementChild) {
         particleContainer.removeChild(particleContainer.lastElementChild);
